943d1dc62fa1f5d48de6d3934f9c68cd0b589406
Артём Олегович <47990465+anpilogov-com@users.noreply.github.com>
2021-10-12 13:52:00 +0300

    buttons

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,25 @@
-"use strict";
-
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const svgo = require("gulp-svgo");
 
-let pathsSrc = {
-  imges: "./images/*/**.svg",
-  styles: "./styles/scss/*/**.scss",
-  watchImges: "./images",
-  watchStyles: "./styles/scss",
-};
-
-let pathsDest = {
-  imges: "./images/opt",
-  styles: "./styles/css",
-};
-
 gulp.task("scss", () => {
   return gulp
-    .src(paths.styles)
+    .src("./styles/scss/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(pathsDest.styles));
+    .pipe(gulp.dest("./styles/css/"));
 });
 
 gulp.task("svgo", () => {
-  return gulp.src(paths.imges).pipe(svgo()).pipe(gulp.dest(pathsDest.imges));
+  return gulp
+    .src("./images/*.svg")
+    .pipe(svgo())
+    .pipe(gulp.dest("./images/opt/"));
 });
 
 gulp.task("watcher", () => {
-  gulp.watch(pathsSrc.watchStyles, gulp.series("scss"));
-  gulp.watch(pathsSrc.watchImges, gulp.series("svgo"));
+  gulp.watch("./styles/scss/", gulp.series("scss"));
+  gulp.watch("./images/", gulp.series("svgo"));
 });
-
-gulp.task("def", gulp.series("watcher", "scss", "svgo"));
