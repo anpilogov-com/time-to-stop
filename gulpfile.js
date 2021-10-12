6a22ee58be391193791237e798b4d646976327a2
Артём Олегович <47990465+anpilogov-com@users.noreply.github.com>
2021-10-12 12:02:32 +0300

    Otimazers for SVG & SCSS
    Add to project a optimizer svg files and scss transpiller. Create watcher task in GulpFile for automate convertion.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,37 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require("gulp-sourcemaps");
+const svgo = require("gulp-svgo");
 
-gulp.task("scss-transpil", () => {
+let pathsSrc = {
+  imges: "./images/*/**.svg",
+  styles: "./styles/scss/*/**.scss",
+  watchImges: "./images",
+  watchStyles: "./styles/scss",
+};
+
+let pathsDest = {
+  imges: "./images/opt",
+  styles: "./styles/css",
+};
+
+gulp.task("scss", () => {
   return gulp
-    .src("./styles/scss/**/*.scss")
+    .src(paths.styles)
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("./styles/css/"));
+    .pipe(gulp.dest(pathsDest.styles));
 });
+
+gulp.task("svgo", () => {
+  return gulp.src(paths.imges).pipe(svgo()).pipe(gulp.dest(pathsDest.imges));
+});
+
+gulp.task("watcher", () => {
+  gulp.watch(pathsSrc.watchStyles, gulp.series("scss"));
+  gulp.watch(pathsSrc.watchImges, gulp.series("svgo"));
+});
+
+gulp.task("def", gulp.series("watcher", "scss", "svgo"));
